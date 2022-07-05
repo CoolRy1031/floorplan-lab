813acd8ae0e594b43e75f4392d44dbe3267087a6
CoolRy <that007guy@hotmail.com>
2022-07-05 16:34:32 -0400

    finish sink and oven

---
+++ src/Oven.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Oven = (props) => {
+  return (
+    <>
+    <div id='Oven'>
+      Oven
+    </div>
+    </>
+  )
+}
+
+export default Oven
---
+++ src/Sink.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Sink = (props) => {
+  return(
+    <>
+    <div id="Sink">
+      Sink
+    </div>
+    </>
+  )
+}
+
+export default Sink
